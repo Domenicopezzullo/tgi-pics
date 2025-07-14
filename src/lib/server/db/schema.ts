7ae96eca442f014c87588eaa244b0b363fcd75e9
Domenicopezzullo <pezzullodomenico712@gmail.com>
2025-07-14 16:23:53 +0200

    india

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -5,8 +5,3 @@ export const images = pgTable('images', {
 	url: text('url').notNull(),
 });
 
-export const users = pgTable('users', {
-	id: serial('id').primaryKey(),
-	name: text('name').notNull(),
-	bio: text('bio').default('Hello am 48 year old man from somalia. Sorry for my bed england. I selled my wife for internet connection for play “conter stirk” game.'),
-});
